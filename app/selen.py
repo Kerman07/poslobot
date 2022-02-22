fe81001fe21b8f532c6692c68a82edbb20253d97
kerman07 <kerman07@live.com>
2022-02-22 13:28:26 +0100

    changed sleep time

--- app/selen.py
+++ app/selen.py
@@ -1,5 +1,6 @@
 import selenium
 import os
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -20,20 +21,17 @@ def get_jobs():
 
     baseUrl = "https://www.mojposao.ba/#!searchjobs;keyword=;page=1;title=all;range=week;location=all;i=32_31_47;lk=Sarajevo;state=all"
     driver.get(baseUrl)
+    time.sleep(5)
     collected = []
-    try:
-        jobs = WebDriverWait(driver, 5).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, "BF0HTNC-hg-l"))
-        )
-        for job in jobs:
-            whole_link = job.find_element(By.TAG_NAME, "a")
-            if whole_link.text:
-                posao = whole_link.text
-                whole_link = whole_link.get_attribute("href").split(";")
-                link = whole_link[0] + ";" + whole_link[-1]
-                hgc = job.find_elements(By.CLASS_NAME, "BF0HTNC-hg-c")[-1]
-                poslodavac = hgc.text
-                collected.append({posao, poslodavac, link})
-    finally:
-        driver.quit()
-        return collected
+    jobs = driver.find_elements(By.CLASS_NAME, "BF0HTNC-hg-l")
+    for job in jobs:
+        whole_link = job.find_element(By.TAG_NAME, "a")
+        if whole_link.text:
+            posao = whole_link.text
+            whole_link = whole_link.get_attribute("href").split(";")
+            link = whole_link[0] + ";" + whole_link[-1]
+            hgc = job.find_elements(By.CLASS_NAME, "BF0HTNC-hg-c")[-1]
+            poslodavac = hgc.text
+            collected.append([posao, poslodavac, link])
+    driver.quit()
+    return collected
