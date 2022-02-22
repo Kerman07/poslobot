39da066d151386440bd584bad8ae6b55fcf7558e
kerman07 <kerman07@live.com>
2022-02-22 12:22:37 +0100

    chrome add

--- app/selen.py
+++ app/selen.py
@@ -6,19 +6,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(
-    executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options
-)
-
-baseUrl = "https://www.mojposao.ba/#!searchjobs;keyword=;page=1;title=all;range=week;location=all;i=32_31_47;lk=Sarajevo;state=all"
-
 
 def get_jobs():
+    chrome_options = webdriver.Options()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(
+        executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options
+    )
+
+    baseUrl = "https://www.mojposao.ba/#!searchjobs;keyword=;page=1;title=all;range=week;location=all;i=32_31_47;lk=Sarajevo;state=all"
     driver.get(baseUrl)
     collected = []
     try:
