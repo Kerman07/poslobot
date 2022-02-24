5cffbaf2d0f01772bc4ad15be7b3656e5a1f6753
kerman07 <kerman07@live.com>
2022-02-24 09:38:55 +0100

    added subscription messages

--- app/routes.py
+++ app/routes.py
@@ -17,12 +17,12 @@ from viberbot.api.viber_requests import (
 from app.utils.scrape import get_jobs
 from app import db
 from app.models import User
-from app.utils.texts import conversation_started, subscribed
+from app.utils.texts import conversation_started, subscribed, categories
 
 
 viber = Api(
     BotConfiguration(
-        name="PythonSampleBot",
+        name="Poslobot",
         avatar="http://site.com/avatar.jpg",
         auth_token=os.environ.get("VIBER_AUTH"),
     )
@@ -38,20 +38,21 @@ def incoming():
     ):
         return Response(status=403)
 
-    # this library supplies a simple way to receive a request object
     viber_request = viber.parse_request(request.get_data())
 
     if isinstance(viber_request, ViberSubscribedRequest):
-        viber.send_messages(viber_request.user.id, [TextMessage(text=subscribed)])
-
-    elif isinstance(viber_request, ViberMessageRequest):
-        message = viber_request.message
-        check_user = User.query.filter_by(receiver=viber_request.sender.id).first()
-        if check_user:
-            return Response(status=500)
+        # check_user = User.query.filter_by(receiver=viber_request.sender.id).first()
+        # if check_user:
+        #     return Response(status=500)
         user = User(receiver=viber_request.sender.id)
         db.session.add(user)
         db.session.commit()
+        viber.send_messages(viber_request.user.id, [subscribed])
+
+    elif isinstance(viber_request, ViberMessageRequest):
+        message = viber_request.message
+        if message == "cat":
+            viber.send_messages(viber_request.sender.id, [categories])
 
     elif isinstance(viber_request, ViberUnsubscribedRequest):
         user = User.query.filter_by(receiver=viber_request.user_id).first()
@@ -61,7 +62,7 @@ def incoming():
     elif isinstance(viber_request, ViberConversationStartedRequest):
         viber.send_messages(
             viber_request.user.id,
-            [TextMessage(text=conversation_started)],
+            [conversation_started],
         )
 
     elif isinstance(viber_request, ViberFailedRequest):
--- app/utils/texts.py
+++ app/utils/texts.py
@@ -1,12 +1,68 @@
-conversation_started = (
-    "Zdravo, ja sam Poslobot, da bi počeli koristiti moje usluge pošaljite mi poruku"
+from viberbot.api.messages.text_message import TextMessage
+
+conversation_started = TextMessage(
+    text="Zdravo, ja sam Poslobot, da bi počeli koristiti moje usluge pošaljite mi poruku"
 )
 
-subscribed = (
-    """Dobro došli!\n
+subscribed = TextMessage(
+    text="""Dobro došli!\n
     Svaki dan u 18:00h ćete dobijati poruku sa objavljenim poslovima toga dana.\n
     Da bi postavili kategorije koje vas interesuju pošaljite cat.\n
     Da bi promjenili lokaciju poslova pošaljite loc.\n
     Da vidite sve dostupne komande pošaljite help.
     """
 )
+
+categories = [
+    TextMessage(
+        text="""Da bi postavili željene kategorije pošaljite cat i listu kategorija odvojenu sa .\n
+    Npr. cat 23.24.37
+    """
+    ),
+    TextMessage(
+        text="""Lista dostupnih kategorija:\n
+        1 Administrativne i slične usluge
+        2 Arhitektonske usluge
+        3 Bankarstvo
+        4 Biotehnologija i farmacija
+        5 Konsalting
+        6 Državna služba i uprava
+        7 Ekonomija i finansije
+        8 Građevinarstvo
+        11 Zanatske usluge
+        12 Menadžment i upravljanje
+        13 Marketing - PR
+        14 Mediji
+        15 Obrazovanje
+        16 Osiguranje
+        17 Ostalo
+        18 Policija - Zaštitarske usluge
+        19 Poljoprivreda - Ribarstvo - Šumarstvo
+        20 Pravo
+        21 Proizvodnja
+        22 Računovodstvo - Revizija
+        23 Socijalne usluge - Neprofitne organizacije
+        24 Telekomunikacije
+        25 Transport - Skladištenje i logika
+        26 Turizam
+        27 Ugostiteljstvo
+        29 Nauka - Istraživački rad
+        31 Energetika
+        32 Elektrotehnika - Mašinstvo
+        33 Zdravstvo
+        34 Komercijala - Prodaja
+        37 Mali oglasi
+        38 Ljepota i zdravlje
+        39 Prehrambena industrija
+        42 Grafički dizajn
+        43 Nekretnine
+        45 Saobraćaj i komunikacije
+        46 Ekologija
+        47 IT
+        48 Grafička industrija
+        49 Ekonomija, finansije i računovodstvo
+        50 Ugostiteljstvo i turizam
+        51 Ljudski resursi
+    """
+    ),
+]
